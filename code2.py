f366b6b0cad8d10c7faea47afd194735c848e3a6
josefina <josefina.astaburuaga@estudiante.uc.cl>
2025-08-28 11:02:36 -0400

    casi final

--- code2.py
+++ code2.py
@@ -1,9 +1,9 @@
 print("Elige un número: tijera = 1 , piedra = 2 , papel = 3 , lagartija = 4 o spock = 5")
-s1 = input()
-s2 = input()
+s1 = int(input())
+s2 = int(input())
 
-if s1==1:
-    if s2==1:
+if s1 == 1:
+    if s2 == 1:
         print("Empate")
     elif s2==2:
         print("Gana piedra")
@@ -41,7 +41,6 @@ elif s1==3:
         print("Gana spock")
     else:
         print("Número inválido")
-
 elif s1==4:
     if s2==1:
         print("Gana tijera")
@@ -53,5 +52,18 @@ elif s1==4:
         print("Empate")
     elif s2==5:
         print("Gana spock")
+    else:
+        print("Número inválido")
+elif s1==5:
+    if s2==1:
+        print("Gana tijera")
+    elif s2==2:
+        print("Gana piedra")
+    elif s2==3:
+        print("Gana spock")
+    elif s2==4:
+        print("Gana spock")
+    elif s2==5:
+        print("Empate")
     else:
         print("Número inválido")
